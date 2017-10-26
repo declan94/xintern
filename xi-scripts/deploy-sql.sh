d796ea693c20003e32f69fbc45b464f99b6e47ff
陈晔 <chenye94@qq.com>
2017-10-26 16:19:09 +0800

    init_data

--- xi-scripts/deploy-sql.sh
+++ xi-scripts/deploy-sql.sh
@@ -8,7 +8,7 @@ fi
 SERVER=x.xiangshixi.cc
 
 scp xintern.sql root@${SERVER}:/root/dist/xi-scripts/
-scp fakedata.sql root@${SERVER}:/root/dist/xi-scripts/
+scp initdata.sql root@${SERVER}:/root/dist/xi-scripts/
 
 ssh root@${SERVER}
 # ssh root@${SERVER} "cd /root/dist/xi-scripts; ./drop_database.sh; ./init_database.sh"
