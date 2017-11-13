0770e0e63a324f7590f30ffd187080dfa2b9a8d3
陈晔 <chenye94@qq.com>
2017-11-13 21:10:17 +0800

    sms log

--- xi-server/src/main/java/thu/declan/xi/server/service/impl/SMSServiceImpl.java
+++ xi-server/src/main/java/thu/declan/xi/server/service/impl/SMSServiceImpl.java
@@ -2,8 +2,8 @@ package thu.declan.xi.server.service.impl;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import thu.declan.xi.server.service.SMSService;
@@ -19,17 +19,19 @@ public class SMSServiceImpl implements SMSService{
     final String ENDPOINT = "http://106.ihuyi.com/webservice/sms.php";
     final String ACCOUNT = "cf_cfwxx";
     final String KEY = "1327a2e6f97bb8f3f3f61e4da5b5033c";
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(SMSServiceImpl.class);
+	
     @Override
     public boolean sendMsg(String phone, String content) {
         StringBuilder params = new StringBuilder("method=Submit");
         try {
+			LOGGER.debug("sending msg to [%s] with content [%s]", phone, content);
             params.append("&account=").append(ACCOUNT)
                     .append("&password=").append(KEY)
                     .append("&mobile=").append(phone)
                     .append("&content=").append(URLEncoder.encode(content, "UTF-8"));
         } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(SMSServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+			LOGGER.error("unsupported encoding exception!");
         }
         HttpRequest.sendGet(ENDPOINT, params.toString());
         return true;        
