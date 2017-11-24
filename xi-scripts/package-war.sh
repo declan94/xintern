142011ff50952019b7a98212fc18e09178baf216
陈晔 <chenye94@qq.com>
2017-11-24 10:29:19 +0800

    api

--- xi-scripts/package-war.sh
+++ xi-scripts/package-war.sh
@@ -18,5 +18,7 @@ SERVER=xiangshixi.cc
 echo "[package war]"
 cd ../xi-server
 sed -i '' "s/localhost:8080/${SERVER}/g" ./src/main/webapp/api-doc/xi-api.yaml
+sed -i '' "s/basePath: \\/api/basePath: \\/backend\\/api/g" ./src/main/webapp/api-doc/xi-api.yaml
 mvn clean package -Dmaven.test.skip=true
 sed -i '' "s/${SERVER}/localhost:8080/g" ./src/main/webapp/api-doc/xi-api.yaml
+sed -i '' "s/basePath: \\/backend\\/api/basePath: \\/api/g" ./src/main/webapp/api-doc/xi-api.yaml
